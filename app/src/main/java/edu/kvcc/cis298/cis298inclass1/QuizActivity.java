0363f18928d7e879f4200b01d4906c330a5db83d
Paul VanderWeele <faytro@hotmail.com>
2017-09-13 16:05:49 -0400

    Finish the single question version of the app.

--- app/src/main/java/edu/kvcc/cis298/cis298inclass1/QuizActivity.java
+++ app/src/main/java/edu/kvcc/cis298/cis298inclass1/QuizActivity.java
@@ -2,12 +2,38 @@ package edu.kvcc.cis298.cis298inclass1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        //use R to reach the view and pull a reference
+        //to the Button we want to use in code.
+        //We will get access to the button in the view by using
+        //the id property that we declared on the layout.
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
