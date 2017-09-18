1973a06649061f2833381aa88c1b9d96546b9b6e
Paul VanderWeele <faytro@hotmail.com>
2017-09-18 16:08:37 -0400

    Add question model to project. Update quizactivity to use new question model. Lots of refactoring. Finish chapter 2.

---
+++ app/src/main/java/edu/kvcc/cis298/cis298inclass1/Question.java
@@ -0,0 +1,33 @@
+package edu.kvcc.cis298.cis298inclass1;
+
+/**
+ * Created by cisco on 9/18/2017.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerIsTrue) {
+        mAnswerTrue = answerIsTrue;
+    }
+
+}
--- app/src/main/java/edu/kvcc/cis298/cis298inclass1/QuizActivity.java
+++ app/src/main/java/edu/kvcc/cis298/cis298inclass1/QuizActivity.java
@@ -4,17 +4,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    /*
+    Array of questions. We send over the resource id from R.java
+    as the first parameter of the constructor.
+    We will use this stored ResourceId as the first
+    parameter of our Question class.
+     */
+    private Question[] mQuestionBank = new Question[] {
+             new Question(R.string.question_oceans, false)
+            ,new Question(R.string.question_mideast, false)
+            ,new Question(R.string.question_africa, false)
+            ,new Question(R.string.question_americas, true)
+            ,new Question(R.string.question_asia, true)
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        //Get a reference to the textview that displays the question
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        updateQuestion();
+
 
         //use R to reach the view and pull a reference
         //to the Button we want to use in code.
@@ -24,7 +48,7 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
@@ -32,8 +56,41 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /**
+                 * This will allow us to cycle the index back to zero if it going to be 5.
+                 */
+                mCurrentIndex = (mCurrentIndex +1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+
+    private void updateQuestion() {
+        //Get the question from the array.
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        //Set the text on the question text view to the string resource.
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+                if (userPressedTrue == answerIsTrue) {
+                    messageResId = R.string.correct_toast;
+                } else  {
+                    messageResId = R.string.incorrect_toast;
+                }
+                //Make the toast like before, but now we only need 1.
+                Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 }
